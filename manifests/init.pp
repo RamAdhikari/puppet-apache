f21fedbda6aae1f4c153098bca020b1e8f85f849
root <root@pmaster.homelab.local>
2021-12-23 00:28:20 +1100

    Service class has been added to ensure httpd adn apache service is runninf and enabled.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,14 @@ class apache(
   String $install_ensure,
   String $config_ensure,
   String $config_path,
+  Enum["running","stopped"] $service_ensure,
+  String $service_name,
+  Boolean $service_enable
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
   Class['::apache::install']
   ->Class['::apache::config']
+  ~>CLass['::apache::service']
 }
